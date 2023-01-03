48afb18da5b713ac40b9d7839c96af2166494496
Blaedge <yakediasuquo@gmail.com>
2023-01-03 11:32:46 -0800

    harf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@ y a new line to standard error
 */
 int main(void)
 {
-	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n")
+	fputs("and that piece of art is useful - Dora Korpar, 2015-10-19,\n")
 		return (0);
 }
