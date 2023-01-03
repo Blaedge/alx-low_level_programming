4d62965f4ef49ee2960405cc16f220ea0b097235
Blaedge <yakediasuquo@gmail.com>
2023-01-03 11:27:02 -0800

    tasked

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Printing a code followed b
+y a new line to standard error
+ *
+ * return: 0
+*/
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-	return (1);
+	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n")
+		return (0);
 }
